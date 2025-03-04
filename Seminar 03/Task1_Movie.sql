084ef2b350274133a86b38348f999b6601ef430c
Serhan Chavdarliev <serhi1334@gmail.com>
2025-03-05 00:50:19 +0200

    added 2 tasks

---
+++ Seminar 03/Task1_Movie.sql
@@ -0,0 +1,26 @@
+--1. Напишете заявка, която извежда имената на актрисите, които са също и
+--продуценти с нетни активи над 10 милиона
+SELECT NAME
+FROM MOVIESTAR
+WHERE GENDER = 'F' 
+AND NAME IN (SELECT NAME FROM MOVIEEXEC WHERE NETWORTH>10000000);
+
+--2. Напишете заявка, която извежда имената на тези актьори (мъже и жени),
+--които не са продуценти.
+SELECT NAME
+FROM MOVIESTAR
+WHERE NAME NOT IN(SELECT NAME FROM MOVIEEXEC);
+
+--3. Напишете заявка, която извежда имената на всички филми с дължина,
+--по-голяма от дължината на филма ‘Star Wars’
+SELECT TITLE
+FROM MOVIE
+WHERE LENGTH > (SELECT LENGTH FROM MOVIE WHERE TITLE='Star Wars');
+
+--4. Напишете заявка, която извежда имената на продуцентите и имената на
+--филмите за всички филми, които са продуцирани от продуценти с нетни
+--активи по-големи от тези на ‘Merv Griffin’
+SELECT TITLE,MOVIEEXEC.NAME
+FROM MOVIEEXEC JOIN MOVIE ON CERT#=MOVIE.PRODUCERC#
+WHERE NETWORTH >(SELECT NETWORTH FROM MOVIEEXEC WHERE NAME = 'Merv Griffin');
+
---
+++ Seminar 03/Task2_Pc.sql
@@ -0,0 +1,51 @@
+--1. Напишете заявка, която извежда производителите на персонални
+--компютри с честота над 500.
+SELECT *
+FROM product
+WHERE model IN (SELECT model FROM pc WHERE speed>500);
+
+--2. Напишете заявка, която извежда код, модел 
+--и цена на принтерите с най-висока цена.
+
+SELECT code,model,price
+FROM printer
+WHERE price >= ALL(SELECT price FROM printer);
+-- ИЛИ
+SELECT code,model,price
+FROM printer
+WHERE price = ALL(SELECT MAX(price) FROM printer);
+
+--3. Напишете заявка, която извежда лаптопите, чиято честота е по-ниска от
+--честотата на всички персонални компютри.
+SELECT *
+FROM laptop
+WHERE speed < ALL(SELECT speed From pc);
+
+-- ИЛИ ТОВА С MIN()
+SELECT *
+FROM laptop
+WHERE speed < (SELECT MIN(speed) From pc);
+
+--4. Напишете заявка, която извежда модела и цената на продукта (PC,
+--лаптоп или принтер) с най-висока цена.
+SELECT model,price
+FROM (SELECT model,price FROM pc 
+UNION SELECT model,price FROM laptop
+UNION SELECT model,price FROM printer) as allTable
+WHERE price >= ALL(SELECT price FROM pc 
+UNION SELECT price FROM laptop
+UNION SELECT price FROM printer);
+
+--5. Напишете заявка, която извежда производителя на цветния принтер с
+--най-ниска цена.
+SELECT maker
+FROM product JOIN printer ON product.model = printer.model
+WHERE color ='y' AND price <= ALL(SELECT price FROM printer WHERE color='y');
+
+--6. Напишете заявка, която извежда производителите на тези персонални
+--компютри с най-малко RAM памет, които имат най-бързи процесори.
+SELECT *
+FROM product JOIN (SELECT * FROM pc WHERE ram <= ALL(SELECT ram FROM pc)) as pc1 
+ON product.model = pc1.model
+WHERE speed >= ALL(SELECT speed FROM pc WHERE ram <= ALL(SELECT ram FROM pc));
+
