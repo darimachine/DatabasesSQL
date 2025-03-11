e9103905dd2c4abb5793356f72b70d2e8d2c1c4c
Serhan Chavdarliev <serhi1334@gmail.com>
2025-03-11 19:53:17 +0200

    added 2 tasks solutions

---
+++ Seminar 04/Task_1_Movie.sql
@@ -0,0 +1,31 @@
+SELECT TITLE,NAME
+FROM MOVIEEXEC JOIN MOVIE ON CERT#=PRODUCERC#
+WHERE NAME = (
+--1. Напишете заявка, която извежда името на продуцента и имената на
+--филмите, продуцирани от продуцента на филма ‘Star Wars’.
+SELECT MOVIEEXEC.NAME
+FROM MOVIEEXEC JOIN MOVIE ON CERT#=PRODUCERC#
+WHERE TITLE='Star Wars');
+
+--2. Напишете заявка, която извежда имената на продуцентите на филмите, в
+--които е участвал ‘Harrison Ford’
+SELECT DISTINCT MOVIEEXEC.NAME
+FROM MOVIEEXEC LEFT JOIN MOVIE ON MOVIEEXEC.CERT#=PRODUCERC#
+WHERE MOVIE.TITLE IN (SELECT MOVIETITLE
+FROM STARSIN
+WHERE STARNAME = 'Harrison Ford');
+
+--3. Напишете заявка, която извежда името на студиото и имената на
+--актьорите, участвали във филми, произведени от това студио, подредени
+--по име на студио.SELECT DISTINCT STUDIONAME, STARNAMEFROM MOVIE JOIN STARSIN ON MOVIE.TITLE = STARSIN.MOVIETITLEORDER BY STUDIONAME;--Напишете заявка, която извежда имената на актьорите, участвали във
+--филми на продуценти с най-големи нетни активи.
+SELECT STARNAME, pr.NETWORTH,STARSIN.MOVIETITLE
+FROM STARSIN JOIN (MOVIE JOIN (SELECT * FROM MOVIEEXEC
+WHERE NETWORTH >= ALL(SELECT NETWORTH FROM MOVIEEXEC)) as pr ON MOVIE.PRODUCERC#=pr.CERT#) 
+ON STARSIN.MOVIETITLE = MOVIE.TITLE;
+
+--5. Напишете заявка, която извежда имената на актьорите, които не са
+--участвали в нито един филм.
+SELECT NAME,MOVIETITLE
+FROM MOVIESTAR LEFT JOIN STARSIN ON NAME=STARSIN.STARNAME
+WHERE STARNAME is null;
---
+++ Seminar 04/Task_2_Pc.sql
@@ -0,0 +1,38 @@
+--1. Напишете заявка, която извежда производител, модел и тип на продукт
+--за тези производители, за които съответният продукт не се продава
+--(няма го в таблиците PC, Laptop или Printer)
+SELECT maker,product.model,product.type
+FROM product LEFT JOIN 
+(SELECT model
+FROM pc
+union
+SELECT model
+FROM laptop
+UNION
+SELECT model
+FROM printer) as models ON product.model = models.model
+WHERE models.model is null;
+
+--2. Намерете всички производители, които правят както лаптопи, така и
+--принтери.
+SELECT maker
+FROM product
+WHERE type = 'laptop'
+INTERSECT
+SELECT maker
+FROM product
+WHERE type = 'printer';
+
+--3. Намерете размерите на тези твърди дискове, които се появяват в два
+--или повече модела лаптопи.
+SELECT hd
+FROM laptop
+GROUP BY hd
+HAVING COUNT(hd) >= 2;
+
+--4. Намерете всички модели персонални компютри, които нямат регистриран
+--производител.
+
+SELECT *
+FROM pc LEFT JOIN product ON product.model=pc.model
+WHERE product.model is null;
