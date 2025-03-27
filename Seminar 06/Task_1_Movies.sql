2d64130adb3807ae1606f84c20de9a8c31657fbe
Serhan Chavdarliev <serhi1334@gmail.com>
2025-03-27 11:28:30 +0200

    only Task_3

---
+++ Seminar 06/Task_1_Movies.sql
@@ -0,0 +1,73 @@
+﻿--1. Напишете заявка, която извежда заглавие и година на всички филми, които са
+--по-дълги от 120 минути и са снимани преди 2000 г. Ако дължината на филма е
+--неизвестна, заглавието и годината на този филм също да се изведат.
+
+SELECT TITLE, year,LENGTH
+FROM MOVIE
+WHERE (LENGTH>120  OR LENGTH is null) AND YEAR<2000;
+
+--2. Напишете заявка, която извежда име и пол на всички актьори (мъже и жени),
+--чието име започва с 'J' и са родени след 1948 година. Резултатът да бъде
+--подреден по име в намаляващ ред.
+
+SELECT NAME,GENDER
+FROM MOVIESTAR
+WHERE NAME LIKE 'J%' AND BIRTHDATE > 1948
+ORDER BY NAME DESC;
+
+--3. Напишете заявка, която извежда име на студио и брой на актьорите,
+--участвали във филми, които са създадени от това студио
+
+SELECT STUDIONAME, COUNT(DISTINCT STARSIN.STARNAME) as num_actors
+FROM MOVIE JOIN STARSIN ON TITLE = MOVIETITLE
+GROUP BY STUDIONAME;
+
+--4. Напишете заявка, която за всеки актьор извежда име на актьора и броя на
+--филмите, в които актьорът е участвал.
+SELECT MOVIESTAR.NAME, COUNT(DISTINCT MOVIETITLE) as num_movies
+FROM MOVIESTAR LEFT JOIN STARSIN ON STARNAME = MOVIESTAR.NAME
+GROUP By MOVIESTAR.NAME;
+
+--5. Напишете заявка, която за всяко студио извежда име на студиото и заглавие
+--на филма, излязъл последно на екран за това студио.
+
+
+SELECT MOVIE.STUDIONAME,TITLE, MOVIE.YEAR
+FROM MOVIE JOIN
+(SELECT STUDIONAME, MAX(YEAR) as year
+FROM MOVIE
+GROUP BY STUDIONAME) as mv ON MOVIE.STUDIONAME = mv.STUDIONAME
+AND mv.year = MOVIE.YEAR;
+
+--5. Напишете заявка, която за всяко студио извежда име на студиото и заглавие
+--на филма, излязъл последно на екран за това студио
+SELECT STUDIONAME,TITLE,YEAR
+FROM MOVIE as m
+WHERE YEAR >= ALL(SELECT YEAR FROM MOVIE WHERE m.STUDIONAME = MOVIE.STUDIONAME)
+ORDER BY STUDIONAME DESC;
+
+--6. Напишете заявка, която извежда името на най-младия актьор (мъж)
+SELECT NAME
+FROM MOVIESTAR
+WHERE GENDER ='m' AND BIRTHDATE=(SELECT MAX(BIRTHDATE) as b
+FROM MOVIESTAR WHERE GENDER = 'm');
+
+--7. Напишете заявка, която извежда име на актьор и име на студио за тези
+--актьори, участвали в най-много филми на това студио.
+
+SELECT STUDIONAME,STARNAME
+FROM STARSIN JOIN MOVIE as mv ON STARSIN.MOVIETITLE = mv.TITLE
+WHERE STARSIN.STARNAME = (SELECT TOP 1 STARNAME
+							FROM STARSIN as innerStarsin JOIN MOVIE as innerMv ON
+							innerStarsin.MOVIETITLE = innerMv.TITLE
+							AND mv.STUDIONAME = innerMv.STUDIONAME
+							GROUP BY STARNAME
+							ORDER BY COUNT(STARNAME) DESC);
+
+--8. Напишете заявка, която извежда заглавие и година на филма, и брой на
+--актьорите, участвали в този филм за тези филми с повече от двама актьори.
+
+SELECT MOVIETITLE, MOVIEYEAR, COUNT(STARNAME) as num_actors
+FROM STARSIN as st
+GROUP BY MOVIETITLE,MOVIEYEAR
+HAVING COUNT(st.STARNAME) >2;
---
+++ Seminar 06/Task_3_Pc.sql
@@ -0,0 +1,38 @@
+﻿
+--1. Напишете заявка, която извежда всички модели лаптопи, за които се
+--предлагат както разновидности с 15" екран, така и с 11" екран.
+
+SELECT model,code,screen
+FROM laptop
+WHERE screen = 11 OR screen = 15;
+--2. Да се изведат различните модели компютри, 
+--чиято цена е по-ниска от найевтиния лаптоп, произвеждан от същия производител.
+
+SELECT DISTINCT pc.model
+FROM pc JOIN product as p_pc ON pc.model = p_pc.model
+WHERE pc.price < ( SELECT MIN(price) as min_price
+				FROM laptop JOIN product as p ON laptop.model =p.model
+				WHERE p_pc.maker = p.maker);
+--3. Един модел компютри може да се предлага в няколко разновидности с
+--различна цена. Да се изведат тези модели компютри, чиято средна цена (на
+--различните му разновид
+
+SELECT pc.model,AVG(price) as avg_price
+FROM pc JOIN product as prod ON pc.model=prod.model
+GROUP BY pc.model
+HAVING AVG(price) < (SELECT MIN(price) as min_price
+					FROM laptop JOIN product as p
+					ON laptop.model=p.model
+					WHERE p.maker = prod.maker
+					);
+
+--4. Напишете заявка, която извежда за всеки компютър код на продукта,
+--производител и брой компютри, които имат цена, по-голяма или равна на
+--неговата.
+
+SELECT code,maker, price, (SELECT COUNT(DISTINCT code)  FROM pc as pc2 
+					WHERE pc1.price <= pc2.price) as num_pc_higher_price
+
+FROM pc as pc1 JOIN product as prod ON pc1.model=prod.model;
+
+
